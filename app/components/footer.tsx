0890763bcc8793e7dfedef16f976584943e98111
luissvision <luiss.will@icloud.com>
2026-05-18 13:58:05 +0200

    Test

--- app/components/footer.tsx
+++ app/components/footer.tsx
@@ -9,11 +9,13 @@ export default function Footer() {
 
         <div>
 
-          <Image
-            src={logo}
-            alt="Novio Technologies"
-            className="w-36 h-auto"
-          />
+          <a href="/">
+            <Image
+              src={logo}
+              alt="Novio Technologies"
+              className="w-36 h-auto"
+            />
+          </a>
 
           <p className="mt-8 max-w-sm text-sm leading-relaxed text-white/50">
             Novio Technologies creates premium websites and digital experiences
@@ -23,7 +25,7 @@ export default function Footer() {
           <div className="mt-8">
 
             <p className="text-lg font-semibold text-white">
-              +49 177 8932219
+              <a href="tel:+491778932219">+49 177 8932219</a>
             </p>
 
             <p className="mt-1 text-sm text-white/45">
@@ -99,7 +101,7 @@ export default function Footer() {
 
             <div>
               <p className="text-white">Email:</p>
-              <p>contact@noviotechnologies.com</p>
+              <p><a href="mailto:contact@noviotechnologies.com">contact@noviotechnologies.com</a></p>
             </div>
 
           </div>
@@ -107,15 +109,17 @@ export default function Footer() {
           <div className="mt-8 flex gap-3">
 
             <div className="flex h-11 w-11 items-center justify-center rounded-full bg-white/10 text-white transition hover:bg-white/15">
-              <svg
-                xmlns="http://www.w3.org/2000/svg"
-                viewBox="0 0 90 90"
-                className="h-6 w-6 fill-white"
-              >
-                <path d="M 62.263 90 H 27.738 C 12.443 90 0 77.557 0 62.263 V 27.738 C 0 12.443 12.443 0 27.738 0 h 34.525 C 77.557 0 90 12.443 90 27.738 v 34.525 C 90 77.557 77.557 90 62.263 90 z M 27.738 7 C 16.303 7 7 16.303 7 27.738 v 34.525 C 7 73.697 16.303 83 27.738 83 h 34.525 C 73.697 83 83 73.697 83 62.263 V 27.738 C 83 16.303 73.697 7 62.263 7 H 27.738 z"/>
+              <a href="https://www.instagram.com/noviotechnologies" target="_blank" rel="noopener noreferrer">
+                <svg
+                  xmlns="http://www.w3.org/2000/svg"
+                  viewBox="0 0 90 90"
+                  className="h-6 w-6 fill-white"
+                >
+                  <path d="M 62.263 90 H 27.738 C 12.443 90 0 77.557 0 62.263 V 27.738 C 0 12.443 12.443 0 27.738 0 h 34.525 C 77.557 0 90 12.443 90 27.738 v 34.525 C 90 77.557 77.557 90 62.263 90 z M 27.738 7 C 16.303 7 7 16.303 7 27.738 v 34.525 C 7 73.697 16.303 83 27.738 83 h 34.525 C 73.697 83 83 73.697 83 62.263 V 27.738 C 83 16.303 73.697 7 62.263 7 H 27.738 z"/>
                 <path d="M 45.255 70.47 c -13.904 0 -25.215 -11.312 -25.215 -25.215 S 31.352 20.04 45.255 20.04 s 25.215 11.312 25.215 25.215 S 59.159 70.47 45.255 70.47 z M 45.255 27.04 c -10.044 0 -18.215 8.171 -18.215 18.215 c 0 10.044 8.171 18.215 18.215 18.215 c 10.044 0 18.215 -8.171 18.215 -18.215 C 63.471 35.211 55.3 27.04 45.255 27.04 z"/>
                 <circle cx="70.557" cy="19.937" r="4.897"/>
               </svg>
+              </a>
             </div>
 
           </div>
